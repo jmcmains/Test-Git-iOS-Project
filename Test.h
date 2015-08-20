d1b8e020a24bcb4894a25222917013e75447d7bb
Jessica Benninger <jessicabenninger@MacBook-Air.local>
2015-08-20 10:44:16 -0400

    added a test class

---
+++ Test.h
@@ -0,0 +1,13 @@
+//
+//  Test.h
+//  git test
+//
+//  Created by Jessica Benninger on 8/20/15.
+//  Copyright (c) 2015 qtimer. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Test : NSObject
+
+@end
---
+++ Test.m
@@ -0,0 +1,13 @@
+//
+//  Test.m
+//  git test
+//
+//  Created by Jessica Benninger on 8/20/15.
+//  Copyright (c) 2015 qtimer. All rights reserved.
+//
+
+#import "Test.h"
+
+@implementation Test
+
+@end
